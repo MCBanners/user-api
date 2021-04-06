e3a87cc3281a89594fd6615e2eca59bbff5d0fef
darbyjack <admin@glaremasters.me>
2021-04-06 12:06:27 -0500

    Maintenance Work

--- src/main/java/com/mcbanners/userapi/controller/UserController.java
+++ src/main/java/com/mcbanners/userapi/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.security.Principal;
 import java.util.Collections;
 import java.util.Map;
 
--- src/main/java/com/mcbanners/userapi/security/WebSecurity.java
+++ src/main/java/com/mcbanners/userapi/security/WebSecurity.java
@@ -11,9 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.http.HttpServletResponse;
 
