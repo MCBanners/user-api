5d42a05afa9844e568aab812516b7cbcab171f85
Glare <admin@glaremasters.me>
2025-04-27 15:11:04 -0500

    chore(deps): update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	id("org.springframework.boot") version "2.7.17"
 	id("io.spring.dependency-management") version "1.1.3"
-	id("com.github.ben-manes.versions") version "0.51.0"
+	id("com.github.ben-manes.versions") version "0.52.0"
 	id("java")
 }
 
@@ -26,7 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-	implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+	implementation("com.github.ben-manes.caffeine:caffeine:3.2.0")
 	implementation("org.mariadb.jdbc:mariadb-java-client:3.1.4")
 	implementation("org.flywaydb:flyway-mysql:9.17.0")
 	implementation("org.mindrot:jbcrypt:0.4")
